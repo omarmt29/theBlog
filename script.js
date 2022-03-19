ddfef3fff16758ac85078e66d5189158c2389ed3
Omar argenis Méndez Torres <omarmendezt29@gmail.com>
2022-03-18 21:42:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function clearContent()
+{
+    document.getElementById("textarea").value="";
+}
+
+function clearText(){
+
+    document.getElementById("nombre").value="";
+    document.getElementById("email").value="";
+    document.getElementById("textarea").value="";
+
+}
+
